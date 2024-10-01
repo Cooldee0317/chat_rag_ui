bce50ff9c7b1c4cdbca590d1d3c555f3493cc68a
Cooldee0317 <cooldeelancer@gmail.com>
2024-10-01 04:37:19 -0700

    [updated] udpated colours array

--- app/page.tsx
+++ app/page.tsx
@@ -91,10 +91,12 @@ export default function Home() {
   const writingStatus = useAppSelector((state) => state.chat.writing_status)
   const chat_history = useAppSelector((state) => state.chat.lists)
   const chatEndRef = useRef<HTMLDivElement | null>(null)
+  const inputEle = useRef<HTMLInputElement | null>(null)
 
   useEffect(() => {
     let id = generateConversationID()
     setConversationID(id)
+    inputEle.current?.focus()
   }, [])
 
   useEffect(() => {
@@ -142,7 +144,7 @@ export default function Home() {
         setCurrentResponseType(response.data.response_type)
         setCurrentSearchQuery(response.data.current_search_query)
         setButtons(response.data.buttons)
-        setColours(response.data?.colours ?? []);
+        setColours(response.data?.colours.reverse() ?? []);
         let msg = {
           content: response.data.response,
           sender: 'bot',
@@ -531,12 +533,13 @@ export default function Home() {
                               </div>
                             </div>
                           )
+                        } else {
+                          return (<div className={`cursor-pointer px-1 h-20 flex justify-center items-center rounded-md swatch_bg`} key={index} onClick={() => {
+                            window.open('https://www.jub.eu/system-solutions-diy/dekorativne-resitve-en/')
+                          }}></div>)
                         }
                       })
                     }
-                    <div className={`cursor-pointer px-1 h-20 flex justify-center items-center rounded-md swatch_bg`} onClick={() => {
-                      window.open('https://www.jub.eu/system-solutions-diy/dekorativne-resitve-en/')
-                    }}></div>
                   </div>
                 </div>
                 <div
@@ -563,6 +566,7 @@ export default function Home() {
                 <input
                   disabled={writingStatus ? true : false}
                   type='text'
+                  ref={inputEle}
                   placeholder={customLanguage?.placeholder}
                   className='py-3 pl-2 pr-14 sm:py-5 sm:px-4 sm:pr-32 rounded-full w-full shadow-custom-inset focus-visible:outline-none'
                   value={message}
